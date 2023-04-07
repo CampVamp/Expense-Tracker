5a5bfc7c223eecc38b89c2f1f17a7e3dc5f448b4
CampVamp <ajayramsaravanan4@gmail.com>
2023-04-07 11:59:15 +0530

    :sparkles: Add Forms in Payment Page

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -1,7 +1,55 @@
+import { Field, Form, Formik } from "formik";
+
+const FieldClass =
+  "border-b-2 border-white bg-transparent placeholder-gray-500 md:text-right font-normal focus:outline-none xl:w-96 p-3 font-subHeading focus:border-green-500 focus:bg-black-100 bg-gray-900 bg-opacity-10 w-full";
+const FieldClassDropDown =
+  "basis-3/4 w-full text-black border-b-2 border-white bg-transparent placeholder-gray-500 md:text-right font-normal focus:outline-none p-3 font-subHeading focus:border-green-500 focus:bg-black-100 bg-gray-900 bg-opacity-10 w-full";
+
+type formData = {
+  paidfor: string;
+  amt: string;
+};
+
 const payment = () => {
+  let initialValues: formData = {
+    paidfor: "",
+    amt: "",
+  };
+
+  const onSubmit = (values: formData) => {
+    console.log("Hello Ajay, the form was submitted!");
+    console.log(values);
+  };
   return (
     <div className="flex items-center justify-center w-screen h-screen">
-      <div>Hello world!!</div>
+      <div>
+        <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          <Form className="flex flex-col justify-center gap-4 w-80">
+            <Field
+              component="select"
+              name="paidfor"
+              className={FieldClassDropDown}
+            >
+              <option value="" disabled hidden>
+                Paid for
+              </option>
+              <option value="Petrol">Petrol</option>
+              <option value="Parking">Parking</option>
+              <option value="Snacks">Snacks</option>
+              <option value="other">Other</option>
+            </Field>
+            <Field
+              className={FieldClass}
+              placeholder="How much did you pay?"
+              name="amt"
+              type="text"
+            ></Field>
+            <button className=" bg-green-600 py-3 rounded-lg" type="submit">
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
